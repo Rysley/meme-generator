be2496d8678ba803ed63676fea4c04d36aa8a0e3
Rysley <ryszardleykam@gmail.com>
2022-10-27 22:34:54 +0200

    form component and styles added

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header";
+import Form from "./Form";
 import Footer from "./Footer";
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <div className="app--wrapper">
       <div className="app--container">
         <Header />
+        <Form />
         <Footer />
       </div>
     </div>
---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Form() {
+  return (
+    <div className="form--container">
+      <input className="form--input" type="text"></input>
+      <input className="form--input" type="text"></input>
+      <button className="form--btn"></button>
+    </div>
+  );
+}
