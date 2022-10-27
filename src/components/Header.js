150049d8e01a7f0dfe473b5ef9aa63cffd30e0a1
Rysley <ryszardleykam@gmail.com>
2022-10-27 22:07:40 +0200

    header and footer styles upgrade

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDumpsterFire } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
   return (
     <div className="header--container">
-      <h3>Header Title</h3>
+      <div className="header--title">
+        <FontAwesomeIcon className="header--icon" icon={faDumpsterFire} />
+        <h2>Meme Generator</h2>
+      </div>
+      <div className="header--subtitle">
+        <h3>React Project #3</h3>
+      </div>
     </div>
   );
 }
