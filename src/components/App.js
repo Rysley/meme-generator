5b47759afbba58afdd85f2f458a6429cbdf58fc7
Rysley <ryszardleykam@gmail.com>
2022-10-30 09:41:21 +0100

    state moved from Form to App to pass props to Form and View

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
 import React from "react";
 import Header from "./Header";
 import Form from "./Form";
-//import View from "./View";
+import memesData from "../assets/memesData";
+import View from "./View";
 import Footer from "./Footer";
 
 export default function App() {
+  const [allMemeImg, setAllMemeImg] = React.useState(memesData.data.memes);
+
+  const [currMeme, setCurrMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "https://i.imgflip.com/1g8my4.jpg",
+  });
+
   return (
     <div className="app--wrapper">
       <div className="app--container">
         <Header />
-        <Form />
+        <Form data={allMemeImg} setCurrMeme={setCurrMeme} />
+        <View src={currMeme.randomImage} />
         <Footer />
       </div>
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,13 @@
 import React from "react";
-import View from "./View";
-import memesData from "../assets/memesData";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRandom } from "@fortawesome/free-solid-svg-icons";
 
-export default function Form() {
-  const [allMemeImg, setAllMemeImg] = React.useState(memesData.data.memes);
-
-  const [currMeme, setCurrMeme] = React.useState({
-    topText: "",
-    bottomText: "",
-    randomImage: "https://i.imgflip.com/1g8my4.jpg",
-  });
-
+export default function Form(props) {
   function handleClick() {
-    const randNum = Math.floor(Math.random() * allMemeImg.length);
-    const { url } = allMemeImg[randNum];
-    setCurrMeme((prevMeme) => {
+    const randNum = Math.floor(Math.random() * props.data.length);
+    const { url } = props.data[randNum];
+    props.setCurrMeme((prevMeme) => {
       return { ...prevMeme, randomImage: url };
     });
   }
@@ -40,7 +31,6 @@ export default function Form() {
           Get a random meme image <FontAwesomeIcon icon={faRandom} />
         </button>
       </div>
-      <View src={currMeme.randomImage} />
     </div>
   );
 }
