6ea8b6ee9d09922d79937484b9c0ef83e6db33f1
Rysley <ryszardleykam@gmail.com>
2022-11-04 16:52:14 +0100

    API call replaced a static data file in APP

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
 import React from "react";
 import Header from "./Header";
 import Form from "./Form";
-import memesData from "../assets/memesData";
+/* import memesData from "../assets/memesData"; */
 import View from "./View";
 import Footer from "./Footer";
 
 export default function App() {
-  const [allMemeImg, setAllMemeImg] = React.useState(memesData.data.memes);
+  const [allMemeImg, setAllMemeImg] = React.useState({});
 
   const [currMeme, setCurrMeme] = React.useState({
     topText: "TOP TEXT",
     bottomText: "BOTTOM TEXT",
     randomImage: "https://i.imgflip.com/1g8my4.jpg",
   });
+
+  React.useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemeImg(data.data.memes));
+  }, []);
 
   function changeMeme() {
     const randNum = Math.floor(Math.random() * allMemeImg.length);
@@ -23,13 +29,10 @@ export default function App() {
 
   function changeText(e) {
     const { name, value } = e.target;
-
-    setCurrMeme((prevInput) => {
-      return {
-        ...prevInput,
-        [name]: value,
-      };
-    });
+    setCurrMeme((prevInput) => ({
+      ...prevInput,
+      [name]: value,
+    }));
   }
 
   return (
